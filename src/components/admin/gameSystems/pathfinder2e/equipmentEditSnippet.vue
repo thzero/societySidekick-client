d3768378ca0d2cde586aaa2ae7d54204b8fbd8fc
thzero <thzero@users.noreply.github.com>
2020-10-08 12:34:58 -0500

    vue lint issues

--- src/components/admin/gameSystems/pathfinder2e/equipmentEditSnippet.vue
+++ src/components/admin/gameSystems/pathfinder2e/equipmentEditSnippet.vue
@@ -2,20 +2,24 @@
 	<table width="100%">
 		<tr>
 			<td>
+				<!-- eslint-disable vue/no-mutating-props -->
 				<VNumberFieldWithValidation
 					ref="level"
 					v-model="value.level"
 					vid="level"
 					:label="$t('forms.equipment.level')"
 				/>
+				<!--eslint-enable-->
 			</td>
 			<td>
+				<!-- eslint-disable vue/no-mutating-props -->
 				<VNumberFieldWithValidation
 					ref="bulk"
 					v-model="value.bulk"
 					vid="bulk"
 					:label="$t('forms.equipment.bulk')"
 				/>
+				<!--eslint-enable-->
 			</td>
 		</tr>
 	</table>
